23a29818d337e4b879b2dda9ae2580664429d865
Udit Gaurav <udit.gaurav@harness.io>
2024-01-31 16:16:06 +0530

    add setup
    Signed-off-by: Udit Gaurav <udit.gaurav@harness.io>

--- bin/install.ps1
+++ bin/install.ps1
@@ -181,7 +181,7 @@ try {
     # Define the service binary to download
     $serviceBinary = @{
         Name = "windows-chaos-infrastructure";
-        DownloadUrl = "https://github.com/uditgaurav/windows-service/raw/master/setup.5/windows-chaos-infrastructure.exe";
+        DownloadUrl = "https://github.com/uditgaurav/windows-service/raw/master/setup.6/windows-chaos-infrastructure.exe";
         Path = "$chaosBasePath\windows-chaos-infrastructure.exe"
     }
 
