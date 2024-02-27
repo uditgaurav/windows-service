a59ac2ff94fd68903a4d2302e3dd694c0536b241
Udit Gaurav <udit.gaurav@harness.io>
2024-02-27 13:09:42 +0530

    update setup scripts
    Signed-off-by: Udit Gaurav <udit.gaurav@harness.io>

--- bin/install.ps1
+++ bin/install.ps1
@@ -6,7 +6,7 @@ param (
     [string]$AccessKey = "",
     [string]$ServerUrl = "",
     [string]$LogDirectory = "C:\\HCE\Logs",
-    [string]$ChaosBasePath = "C:\\HCE"
+    [string]$ChaosBasePath = "C:\\HCE",
     [int]$TaskPollIntervalSeconds = 5,
     [int]$TaskUpdateIntervalSeconds = 5,
     [int]$UpdateRetries = 5,
